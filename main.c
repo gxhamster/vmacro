4c83b5ceed76c67121fa40cce84188b356f9c7f6
gxhamster <gxhamster@pop-os.localdomain>
2021-11-25 12:02:53 +0500

    Words are now deleted according to cursor position and other improvements

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
 #define ERROR(s) fprintf(stderr, "ERROR: %s\n", s)
@@ -20,7 +21,10 @@ typedef struct {
 } Action;
 
 
-typedef struct { char *start; char *end;
+// Word points to places on the actual src
+typedef struct { 
+    char *start; 
+    char *end;
     size_t len;
 } Word;
 
@@ -37,9 +41,11 @@ typedef struct {
 // Declarations
 Line process_line(char *buf_src, size_t size);
 Action process_actions(char *action_str, size_t len);
-Line *line_delete_word(Line *l, size_t word_idx);
+Line *line_delete_word_at_cursor(Line *l);
 Line *eval_action_on_line(Line *l, Action *a);
 char *read_from_file(FILE *fp);
+Line *next_word_end(Line *l);
+Line *next_word_start(Line *l);
 
 
 Action process_actions(char *action_str, size_t len)
@@ -48,7 +54,7 @@ Action process_actions(char *action_str, size_t len)
         ERROR("Action string is NULL");
         exit(-1);
     }
-    
+
     Action a;
     Movement m;
     char num_buffer[10] = {0};
@@ -86,53 +92,103 @@ Action process_actions(char *action_str, size_t len)
     return a;
 }
 
+// Move to end of word 
+Line *next_word_end(Line *l)
+{
+
+    Word *cur_word = &l->words[l->cur_word_idx];
+    Word *next_word;
+    // If cursor is not at end of word move to end of current word
+    if (l->cursor < cur_word->end) {
+        l->cursor = cur_word->end;
+    } else {
+        if (l->cur_word_idx + 1 > l->n_words - 1) {
+            ERROR("Reached end of words on line");
+            return NULL;
+        }
+        next_word = &l->words[l->cur_word_idx + 1];
+        l->cursor = next_word->end; 
+        l->cur_word_idx += 1; 
+    }
+
+    return l;
+}
+
+
+// Move cursor to next start of next word
+Line *next_word_start(Line *l)
+{
+    if (l->cur_word_idx + 1 > l->n_words - 1) {
+        ERROR("Reached end of words on line");
+        return NULL;
+    }
+    char *start = l->words[l->cur_word_idx+1].start;      
+    l->cursor = start;
+    l->cur_word_idx += 1; 
+
+    return l;
+}
 
 // Delete a word in a line at a certain index
 // This will also create a new src str the line points to
-Line *line_delete_word(Line *l, size_t word_idx)
+Line *line_delete_word_at_cursor(Line *l)
 {
+
     if (l == NULL) {
         ERROR("Line is NULL");
         return NULL;
     }
-    if (word_idx > l->n_words-1 || word_idx < 0) {
+    size_t word_idx = l->cur_word_idx;
+    if (word_idx > l->n_words-1) {
         ERROR("Word index out of range");
         return NULL;
     }
-    
+
     Word *word, *word_next;
     word = &l->words[word_idx];
     word_next = (word_idx+1 < l->n_words - 1) ? &l->words[word_idx+1] : NULL;
+    
+    bool is_last_word = (!word_next) ? true : false;
 
-
-    // hello world again???
-    char new_src[l->len - word->len];
+    char *start;
+    char *end;
+    // If in the word delete upto end
+    if (l->cursor < word->end && l->cursor > word->start) {
+        start = l->cursor;
+        end = word->end;
+    } else if (l->cursor == word->start) {
+        start = word->start;
+        end = word->end;
+    } else {
+        ERROR("Cursor is not at this word");
+        return NULL;
+    }
+    
+    size_t del_len = end - start + 1;
+    char new_src[l->len - del_len];
     size_t new_src_count = 0;
-    char *p;
+    
+    if (!is_last_word) {
+        end = word_next->start - 1;
+    }
+
     size_t i;
     for (i = 0; i < l->len; i++) {
-        p = &l->src[i];
-        if (word_next == NULL) {
-            // If it is last word
-            if (p < word->start || p > word->end) {
-                new_src[new_src_count] = *p;
-                new_src_count++;
-            }
-        } else if (p < word->start || p >= word_next->start) {
-            // Only copy from this word to start of next word
-            new_src[new_src_count] = *p;
+        if (&l->src[i] < start || &l->src[i] > end) {
+            new_src[new_src_count] = l->src[i];
             new_src_count++;
-        } 
+        }
     }
     new_src[new_src_count] = '\0';
-    word->start = NULL;
-    word->end = NULL;
-    word->len = 0;
-    
+    char *cursor_pos = l->cursor;
+    char *src = l->src;
+    size_t offset = cursor_pos - src - 1;
+
     // Reproduce the line structure becaus src is changed
     Line l_new = process_line(new_src, new_src_count);
     *l = l_new;
 
+    l->cursor = l->src + offset;
     return l;
 }
 
@@ -143,7 +199,7 @@ Line *eval_action_on_line(Line *l, Action *a)
         l->cur_word_idx += (a->mov.count + l->cur_word_idx > l->n_words) ? l->n_words-1 : a->mov.count; 
         l->cursor = l->words[l->cur_word_idx].start;
     } else if (a->command == 0 && a->mov.command == WORD_BACKWARD) {
-        l->cur_word_idx -= (l->cur_word_idx - a->mov.count < 0 ) ? 0 : a->mov.count; 
+        l->cur_word_idx -= ((int)(l->cur_word_idx - a->mov.count) < 0 ) ? 0 : a->mov.count; 
         l->cursor = l->words[l->cur_word_idx].start;
     }
 }
@@ -166,11 +222,11 @@ Line process_line(char *buf_src, size_t size)
     line.cur_word_idx = 0;
     line.n_words = 0;
 
+
+    // TODO: implement way to find words without using strtok
     char *token;
     size_t token_size;
     const char *delim = "-.\t ";
-    // FIXME: Pointer in the words no longer point to src
-    // because we duplicated it 
     token = strtok(buf, delim);
     while (token != NULL) {
         // printf("%p %s\n", token, token);
@@ -213,9 +269,9 @@ int main()
 {
     char *str = "hello world again it's me Mario";
     Line l = process_line(str, strlen(str));   
-    line_delete_word(&l, 5);
-    line_delete_word(&l, 1);
-    line_delete_word(&l, 0);
+    // l.cursor = l.src + 26;
+    next_word_start(&l);
+    line_delete_word_at_cursor(&l);
     printf("Line: %s\n", l.src);
 
 }
